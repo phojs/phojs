96a9638f43b82f361723bf18c597ff369aa4380a
Anton Boyko <anton@layer11.ai>
2022-09-21 19:11:06 +0300

    examples: wip

--- examples/basic-project/main.pho.js
+++ examples/basic-project/main.pho.js
@@ -1,5 +1,5 @@
 const pho = require('phojs')
 
-const main = pho.category('basic params', '')
-main.field('name', 'string', 'project name')
+const main = pho.category('basic', '')
+main.field('name', 'string', 'project name').required()
 main.field('version', 'number', 'project version(inc float)', 1)
